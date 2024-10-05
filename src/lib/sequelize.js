271dff6015117f61eeb8cee88766122c87cefa52
alicedev94 <alicedev94@gmail.com>
2024-10-04 21:13:36 -0400

    config donweb

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize('rifaslav_test', 'rifaslav_rifa', 'a%AET[0v55u-'
   host: '205.209.113.202', // IP de tu servidor MySQL
   dialect: 'mysql',
   port: 3306, // Puerto por defecto de MySQL
-  logging: false,
+  // logging: false,
   dialectOptions: {
     // Tus opciones de mysql2 aquí
   },
