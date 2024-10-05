96fb038d823768b024da07b53929b65332636006
alicedev94 <alicedev94@gmail.com>
2024-10-04 22:11:32 -0400

    s

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -3,18 +3,8 @@ const { Sequelize } = require("sequelize");
 const setupModels = require("../models/index");
 require("dotenv").config();
 
-// const sequelize = new Sequelize('rifaslav_test', 'rifaslav_rifa', 'a%AET[0v55u-', {
-//   host: '205.209.113.202', // IP de tu servidor MySQL
-//   dialect: 'mysql',
-//   port: 3306, // Puerto por defecto de MySQL
-//   // logging: false,
-//   dialectOptions: {
-//     // Tus opciones de mysql2 aquí
-//   },
-// });
-
-const sequelize = new Sequelize('test', 'root', '', {
-  // host: '205.209.113.202', // IP de tu servidor MySQL
+const sequelize = new Sequelize('rifaslav_test', 'rifaslav_rifa', 'a%AET[0v55u-', {
+  host: '205.209.113.202', // IP de tu servidor MySQL
   dialect: 'mysql',
   port: 3306, // Puerto por defecto de MySQL
   // logging: false,
@@ -22,6 +12,16 @@ const sequelize = new Sequelize('test', 'root', '', {
     // Tus opciones de mysql2 aquí
   },
 });
+
+// const sequelize = new Sequelize('test', 'root', '', {
+//   // host: '205.209.113.202', // IP de tu servidor MySQL
+//   dialect: 'mysql',
+//   port: 3306, // Puerto por defecto de MySQL
+//   // logging: false,
+//   dialectOptions: {
+//     // Tus opciones de mysql2 aquí
+//   },
+// });
 setupModels(sequelize);
 sequelize.sync();
 
