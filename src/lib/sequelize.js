2686b810a04957163d002690f6288733ec95a38f
alicedev94 <alicedev94@gmail.com>
2024-10-04 21:12:24 -0400

    config donweb

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const setupModels = require("../models/index");
 require("dotenv").config();
 
-const sequelize = new Sequelize('test', 'root', '', {
+const sequelize = new Sequelize('rifaslav_test', 'rifaslav_rifa', 'a%AET[0v55u-', {
   host: '205.209.113.202', // IP de tu servidor MySQL
   dialect: 'mysql',
   port: 3306, // Puerto por defecto de MySQL
